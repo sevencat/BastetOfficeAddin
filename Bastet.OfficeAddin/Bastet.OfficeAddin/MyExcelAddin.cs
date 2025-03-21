53fff9744f40a417970e199894f19640f9e86fd1
sevencat <bastet.wang@aliyun.com>
2025-03-21 20:46:59 +0800

    使用com来写office addin 这个可以从2010支持到最新。也不用安装vsto

---
+++ Bastet.OfficeAddin/Bastet.OfficeAddin/MyExcelAddin.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using NetOffice.ExcelApi.Tools;
+using NetOffice.Tools;
+
+
+namespace Bastet.OfficeAddin;
+
+//e4866de5-adb8-60d8-1d7c-463da09e39d8
+[ComVisible(true)]
+[Guid("e4866de5-adb8-60d8-1d7c-463da09e39d8")]
+[ProgId("Bastet.OfficeAddin.MyExcelAddin")]
+[COMAddin("Bastet Word Addin", "Wordaddin By Bastet", LoadBehavior.LoadAtStartup)]
+public class MyExcelAddin : COMAddin
+{
+	public MyExcelAddin()
+	{
+		this.OnConnection += Addin_OnConnection;
+		OnStartupComplete += Addin_OnStartupComplete;
+		OnDisconnection += Addin_OnDisconnection;
+	}
+
+	private void Addin_OnConnection(object application, ext_ConnectMode connectMode, object addInInst, ref Array custom)
+	{
+	}
+
+	private void Addin_OnStartupComplete(ref Array custom)
+	{
+		Console.WriteLine("Excel Version is {0}", Application.Version);
+	}
+
+	private void Addin_OnDisconnection(ext_DisconnectMode removeMode, ref Array custom)
+	{
+	}
+}
---
+++ Bastet.OfficeAddin/Bastet.OfficeAddin/MyWordAddin.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using NetOffice.Tools;
+using NetOffice.WordApi;
+using NetOffice.WordApi.Tools;
+
+namespace Bastet.OfficeAddin
+{
+	[ComVisible(true)]
+	[Guid("9777f77a-f286-49d6-817b-0500c95b89c2")]
+	[ProgId("Bastet.OfficeAddin.MyWordAddin")]
+	[COMAddin("Bastet Word Addin", "Wordaddin By Bastet", LoadBehavior.LoadAtStartup)]
+	public class MyWordAddin : COMAddin
+	{
+		public MyWordAddin()
+		{
+			this.OnConnection += MyAddin_OnConnection;
+		}
+
+		private void MyAddin_OnConnection(object application, ext_ConnectMode connectMode, object addInInst,
+			ref Array custom)
+		{
+			this.Application.DocumentOpenEvent += Application_DocumentOpenEvent;
+		}
+
+		private void Application_DocumentOpenEvent(Document doc)
+		{
+			using (doc)
+			{
+				// start working with the document
+			}
+		}
+	}
+}
